84f3d628e7a382333d228adfa1260b73eafbd0b9
Jan Molak <jan.molak@smartcodeltd.co.uk>
2016-05-10 19:29:09 +0100

    Corrected "snake_case" to "kebab-case"
    The naming style angular uses for the attributes comes from Lisp and is called ["kebab-case"](http://c2.com/cgi/wiki?KebabCase).
    Docs described it as "snake_case", which might be confusing to the reader.

--- counter.js
+++ counter.js
@@ -2,7 +2,7 @@
   @toc
 
 @param {Object} scope (attrs that must be defined on the scope (i.e. in the controller) - they can't just be defined in the partial html). REMEMBER: use snake-case when setting these on the partial!
-@param {Object} attrs REMEMBER: use snake-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
+@param {Object} attrs REMEMBER: use kebab-case when setting these on the partial! i.e. my-attr='1' NOT myAttr='1'
 
 @usage
 
